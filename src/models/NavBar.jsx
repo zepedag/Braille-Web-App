58318f113a19ece13859d8e1f097e552d0be5e0d
Alejandro Zepeda <humberto.zepedagz@udlap.mx>
2025-02-11 15:57:36 -0600

    navbar name

--- src/models/NavBar.jsx
+++ src/models/NavBar.jsx
@@ -4,7 +4,7 @@ import "../styles/Navbar.css"; // Importa el archivo CSS para este componente
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-title">Mi App</div>
+      <div className="navbar-title">Braille UDLAP</div>
       <div className="navbar-buttons">
         <button className="navbar-button">Regleta Virtual</button>
         <button className="navbar-button">Máquina de Perkins</button>
