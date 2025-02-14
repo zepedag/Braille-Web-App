c3460fa2a7641c27f75d2fbda5e4895b9c93f2fd
Alejandro Zepeda <humberto.zepedagz@udlap.mx>
2025-02-13 18:37:40 -0600

    BraillecConverter.css was added

--- src/models/BrailleConverter.jsx
+++ src/models/BrailleConverter.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import brailleDict from "../braille-resources/brailleDictionary";
+import "../styles/BrailleConverter.css";
 
 const BrailleConverter = () => {
   const [inputText, setInputText] = useState("");
