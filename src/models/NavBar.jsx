413d90c33be547e7e71100a6ec395890c6c7c0bf
Alejandro Zepeda <humberto.zepedagz@udlap.mx>
2025-03-07 12:38:01 -0600

    description labels

--- src/models/NavBar.jsx
+++ src/models/NavBar.jsx
@@ -4,16 +4,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ theme, setTheme }) => {
   return (
-    <nav className="navbar">
+    <nav className="navbar" role="navigation">
       <div className="navbar-title">Braille UDLAP</div>
       <div className="navbar-buttons">
-        <Link to="/" className="navbar-button">Inicio</Link>
-        <Link to="/convert" className="navbar-button">Convertir a Braille</Link>
-        <Link to="/slate" className="navbar-button">Regleta Braille</Link>
-        <Link to="/wordbank" className="navbar-button">WordBank</Link>
+        <Link to="/" className="navbar-button" aria-label="Ir a Inicio">Inicio</Link>
+        <Link to="/convert" className="navbar-button" aria-label="Ir al Convertidor a Braille">Convertidor a Braille</Link>
+        <Link to="/slate" className="navbar-button" aria-label="Ir a la Regleta Braille">Regleta Virtual</Link>
+        <Link to="/wordbank" className="navbar-button" aria-label="Ir al Banco de Palabras">Banco de Palabras</Link>
         <button
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
           className="navbar-button"
+          aria-label={`Activar ${theme === "dark" ? "modo claro" : "modo oscuro"}`}
         >
           {theme === "dark" ? "Modo Claro" : "Modo Oscuro"}
         </button>
