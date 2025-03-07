9e2204d9c9616d65b6bc0e0b7cd39c58e7db8a2f
3str3ll4V3r <156131289+3str3ll4V3r@users.noreply.github.com>
2025-03-07 12:39:52 -0600

    Switch dark mode

--- src/models/NavBar.jsx
+++ src/models/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../styles/Navbar.css"; 
+import "../styles/Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = ({ theme, setTheme }) => {
@@ -7,16 +7,29 @@ const Navbar = ({ theme, setTheme }) => {
     <nav className="navbar">
       <div className="navbar-title">Braille UDLAP</div>
       <div className="navbar-buttons">
-        <Link to="/" className="navbar-button">Inicio</Link>
-        <Link to="/convert" className="navbar-button">Convertir a Braille</Link>
-        <Link to="/slate" className="navbar-button">Regleta Braille</Link>
-        <Link to="/wordbank" className="navbar-button">WordBank</Link>
-        <button
-          onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-          className="navbar-button"
-        >
-          {theme === "dark" ? "Modo Claro" : "Modo Oscuro"}
-        </button>
+        <Link to="/" className="navbar-button">
+          Inicio
+        </Link>
+        <Link to="/convert" className="navbar-button">
+          Convertir a Braille
+        </Link>
+        <Link to="/slate" className="navbar-button">
+          Regleta Braille
+        </Link>
+        <Link to="/wordbank" className="navbar-button">
+          WordBank
+        </Link>
+        <div className="theme-switch">
+          <input
+            type="checkbox"
+            id="theme-toggle"
+            checked={theme === "dark"}
+            onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+          />
+          <label htmlFor="theme-toggle" className="theme-switch-label">
+            <span className="theme-switch-slider"></span>
+          </label>
+        </div>
       </div>
     </nav>
   );
